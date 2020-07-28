fae7629c5abbe303cf989dfd9f6468b7daed37cc
shaowenzhang <zhangshaowen66@aliyun.com>
2020-07-28 17:13:10 +0800

    新建react框架

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const {createProxyMiddleware  } = require('http-proxy-middleware');
-
+// 代理解决跨域问题
 module.exports = function (app) {
     app.use(createProxyMiddleware('/api', {
         target: 'http://59.36.173.92:8088/',
